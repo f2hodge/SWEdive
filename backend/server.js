88c438b7d6124ad41cc0017c5e8c0b8a1876dea6
Ryan Hodge <f2hodge@gmail.com>
2022-03-13 19:43:14 -0400

    small detail adjustments

--- backend/server.js
+++ backend/server.js
@@ -18,4 +18,4 @@ app.use('/api/users', require('./routes/userRoutes'));
 
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server started on port ${port} (server.js)`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
