1f6ac1a319517de2860ee14cb6e56612370cf43d
Ryan Hodge <f2hodge@gmail.com>
2022-03-15 13:00:32 -0400

    frontend auth functionality created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from './components/Header';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Changer from './pages/Changer';
-import ExamDetail from './pages/ExamDetail';
+// import Changer from './pages/Changer';
+//import ExamDetail from './pages/ExamDetail';
 
 function App() {
   return (
@@ -17,11 +19,12 @@ function App() {
             <Route path='/' element={<Dashboard />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
-            <Route path='/admin' element={<Changer />} />
-            <Route path='/examview' element={<ExamDetail />} />
+            {/* <Route path='/admin' element={<Changer />} /> */}
+            {/* <Route path='/examview' element={<ExamDetail />} /> */}
           </Routes>
         </div>
       </Router>
+      <ToastContainer />
     </>
   );
 }
